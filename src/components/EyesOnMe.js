b6be80a4598b54c87fd9577784a9b81febea6a90
mckaymatt3 <mckaymatt@outlook.com>
2022-02-24 11:32:59 -0500

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe () {
+    function blur (event) {
+        console.log("Lock eyes from across the room")
+    }
+    function focus (event) {
+        console.log("Down the drinks as the rhythms boom")
+    }
+
+    return ( 
+        <div>
+            <button onBlur={blur} onFocus={focus}> 
+                Basic
+            </button>
+        </div>
+     );
+}
+
+export default EyesOnMe ;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad () {
+    
+    function changePassword (event) {
+        console.log("Entering password:", `${event.target.value}`)
+    }
+    return ( 
+        <div>
+            <input type="password" onChange={changePassword} /> 
+        </div>
+    );
+}
+
+export default Keypad;
